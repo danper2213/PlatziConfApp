9572978a188fce83cfd358ef5dc259846c89be5e
Daniel Perez <desarrollosupervisa@gmail.com>
2020-04-12 16:34:08 -0500

    Fragments creados

---
+++ app/src/main/java/com/danpern/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.danpern.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danpern.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
